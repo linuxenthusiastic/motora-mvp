b40333984b57a0d70ddfb9dea6cf6c5af3a2458f
linuxenthusiastic <mbitakrec@gmail.com>
2026-06-23 16:19:21 -0400

    fix: add delete cache cuando se crea un nuevo elemento basicamente el cache queda obsoleto durante 60s para produccion es incomprensible solucionando con un limpiador de cache cada vez que sea crea un nuevo elemento obligando a hacer una peticion completa nuevamente

--- app/catalog/router.py
+++ app/catalog/router.py
@@ -6,6 +6,7 @@ from app.catalog.models import CarModel
 from app.catalog.repository import CarModelRepository
 from app.catalog.schemas import CarModelCreate, CarModelResponse
 from app.shared.db.session import get_db
+from app.shared.redis import get_cached, redis_client, set_cached
 
 router = APIRouter(prefix="/catalog", tags=["catalog"])
 
@@ -18,10 +19,19 @@ def create_car_model(
 ):
     repo = CarModelRepository(db)
     new_car_model = repo.create_car_model(brand=car_model.brand, model=car_model.model)
+    redis_client.delete("catalog:all")
     return new_car_model
 
 
 @router.get("/", response_model=list[CarModelResponse])
 def get_all_car_model(db: Session = Depends(get_db)):
+    cached = get_cached("catalog:all")
+    if cached is not None:
+        return cached
     repo = CarModelRepository(db)
-    return repo.get_all_car_models()
+    car_models = repo.get_all_car_models()
+    car_models_dict = [
+        {"id": c.id, "brand": c.brand, "model": c.model} for c in car_models
+    ]
+    set_cached("catalog:all", car_models_dict)
+    return car_models_dict
